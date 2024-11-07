75460bcc51e4576910bf4e39b07df60bb12de9c6
Katerina-il <kkaterina.iln@gmail.com>
2024-11-07 14:24:07 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,5 @@
+my_string=input("произвольный текст: ") #"Практическая работа по уроку 'Организация программ и методы строк'"
+print(len(my_string)) # количество введенных символов
+print(my_string.upper())  # в верхнем регистре
+print(my_string.lower()) # в нижнем регистре
+print(my_string.replace(_old:=" ",_new:=""))
